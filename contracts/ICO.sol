b5d8ccfc2eba948083f55c4757ff89ace4983c43
Xanderrupy <132014285+Xanderrupy@users.noreply.github.com>
2023-05-08 14:31:10 -0700

    fix: fix bug occurs in compiling

--- contracts/ICO.sol
+++ contracts/ICO.sol
@@ -1,82 +1,61 @@
-pragma solidity >=0.8.17;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "./Token.sol";
+pragma solidity ^0.8.0;
+
+import "./ICOToken.sol";
 
 contract ICO {
-    uint public constant TOKEN_SUPPLY = 1000000 * 10 ** 18; // 1 million tokens
-    uint public constant RATE = 1000;
-    uint public constant DAYS = 1;
-    uint public constant START = 1707312000;
-    uint public constant END = START.add(DAYS * 1 days);
-    uint public constant SOFTCAP = 0.1;
-    uint public constant HARDCAP = 1;
-    uint public constant MINPURCHASE = 0.01;
-    uint public constant MAXPURCHASE = 0.05;
-    uint public raisedAmount = 0;
-    // bool public isClosed;
+    ICOToken public token;
+    address public owner;
+    mapping(address => uint256) public deposit;
+    uint256 public softCap;
+    uint256 public hardCap;
+    uint256 public minPurchaseAmount;
+    uint256 public maxPurchaseAmount;
+    bool public isICOActive;
 
-    mapping(address => uint) public balances;
+    event Deposit(address indexed from, uint256 value);
+    event Withdraw(address indexed to, uint256 value);
+    event Claim(address indexed to, uint256 value);
 
-    event Deposit(address indexed investor, uint amount);
-    event Withdraw(address indexed investor, uint amount);
-    event Claim(address indexed investor, uint amount);
+    constructor(ICOToken _token, uint256 _softCap, uint256 _hardCap, uint256 _minPurchaseAmount, uint256 _maxPurchaseAmount) {
+        token = _token;
+        owner = msg.sender;
+        softCap = _softCap;
+        hardCap = _hardCap;
+        minPurchaseAmount = _minPurchaseAmount;
+        maxPurchaseAmount = _maxPurchaseAmount;
+        isICOActive = true;
+    }
 
-    modifier isPossible() {
-        require(now >= START, "ICO has not yet started.");
-        require(now <= END, "ICO has ended.");
-        // require(!isClosed, "ICO is closed.");
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function.");
         _;
     }
 
-    constructor(address _tokenAddr) public {
-        require(_tokenAddr != 0, "Must have account");
-        token = Token(_tokenAddr);
-    }
-
-    function deposit(uint tokens) public payable isPossible {
-        require(msg.value >= MINPURCHASE, "Deposit amount is below minimum.");
-        require(msg.value <= MAXPURCHASE, "Deposit amount is above maximum.");
-        // require(
-        //     msg.value.add(balances[msg.sender]) <= hardCap,
-        //     "Deposit amount exceeds hard cap."
-        // );
-        uint BNBAmount = tokens.div(RATE);
-        
-        raisedAmount = raisedAmount.add(BNBAmount);
-        token.transfer(msg.sender, tokens);
-
-        balances[msg.sender] += tokens;
+    function depositFunds() public payable {
+        require(isICOActive, "ICO is not active.");
+        require(msg.value >= minPurchaseAmount && msg.value <= maxPurchaseAmount, "Invalid purchase amount.");
+        deposit[msg.sender] += msg.value;
         emit Deposit(msg.sender, msg.value);
     }
 
-    function withdraw() public {
-        require(
-            (now > END && balances[msg.sender] > 0),
-            "Withdrawals not allowed at this time."
-        );
-        uint tokens = balances[msg.sender];
-        uint BNBAmount = tokens.div(RATE);
-
-        raisedAmount = raisedAmount.sub(BNBAmount);
-        token.transferFrom(msg.sender, address(this), tokens);
-
-        balances[msg.sender] = 0;
-        // payable(msg.sender).transfer(amount);
-        emit Withdraw(msg.sender, amount);
+    function withdrawFunds() public onlyOwner {
+        require(!isICOActive, "ICO is still active.");
+        require(address(this).balance >= softCap, "Soft cap not reached.");
+        payable(owner).transfer(address(this).balance);
+        emit Withdraw(owner, address(this).balance);
     }
 
-    function claim(unit tokens) public {
-        require(
-            (( now > END && balances[msg.sender] > tokens && raisedAmount >= SOFTCAP )||( raisedAmount >= HARDCAP )),
-            "Withdrawals not allowed at this time."
-        );
+    function claimTokens() public {
+        require(!isICOActive, "ICO is still active.");
+        require(address(this).balance < softCap, "Soft cap reached.");
+        uint256 depositAmount = deposit[msg.sender];
+        require(depositAmount > 0, "No deposit found for this address.");
+        deposit[msg.sender] = 0;
+        token.transfer(msg.sender, depositAmount);
+        emit Claim(msg.sender, depositAmount);
+    }
 
-        uint BNBAmount = tokens.div(RATE);
-        
-        raisedAmount = raisedAmount.sub(BNBAmount);
-        token.transferFrom(msg.sender, address(this), tokens);
-
-        balances[msg.sender] -= tokens;
-        emit Claim(msg.sender, amount);
+    function endICO() public onlyOwner {
+        isICOActive = false;
     }
 }
