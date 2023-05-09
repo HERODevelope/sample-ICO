b96aba2286d4087fb9f5fd810f263c8cdb7cc10d
henrywinston <henrywinston87@gmail.com>
2023-05-09 14:12:47 +0900

    'clientv1'

---
+++ client/pages/_app.tsx
@@ -0,0 +1,22 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { ToastContainer, toast } from "react-toastify";
+
+
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <div>
+      <Head>
+        <title>ICO Offering</title>
+        <link rel="icon" href="/favicon.ico" />
+         
+      </Head>
+      <ToastContainer/>
+      <Component {...pageProps} />
+    </div>
+  );
+}
+
+export default MyApp;
